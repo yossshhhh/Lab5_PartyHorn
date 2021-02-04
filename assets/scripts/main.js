ff29fdae8bb0a755f663307d071eb6623ec4b15a
yossshhhh <55852579+yossshhhh@users.noreply.github.com>
2021-02-04 10:43:20 -0800

    volume image adjustments drafted

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,29 @@
 // main.js
 
-// TODO
+let volNum = document.getElementById("volume-number");
+let volSlider = document.getElementById("volume-slider");
+let volImg = document.getElementById("volume-image");
+let sound = document.getElementById("horn-sound");
+
+function updateImage(vol) {
+  vol = vol % 100; //adjust in case input is greater than 100
+  sound.volume = vol/100; //set volume percent of 100
+  
+  if(vol > 66) { //67-100
+    volImg.src = "./assets/media/icons/volume-level-3.svg";
+  }
+  else if(vol > 33) { //34-66
+    volImg.src = "./assets/media/icons/volume-level-2.src";
+  }
+  else if(vol > 0) { //1-33
+    volImg.src = "./assets/media/icons/volume-level-1.src";
+  }
+  else { //0
+    volImg.src = "./assets/media/icons/volume-level-0-.src";
+  }
+}
+
+  
+  
+    
+  
